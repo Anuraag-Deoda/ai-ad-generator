7385cbc704c0a5d53eb199facd36f0ea404f9b76
Your anurag <anuraagdeoda810@gmail.com>
2025-06-11 15:12:09 +0530

    🎉 Clean initial commit without tailwindcss.exe

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100 text-xl">
+      <h1 className="text-blue-600 font-bold">✅ Tailwind CSS is working!</h1>
+    </div>
+  );
+}
